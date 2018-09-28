350acc7aaa33c6a0988d31ac81ad96d4af4dd1af
ElOmai <43100537+ElOmai@users.noreply.github.com>
2018-09-28 13:48:35 -0400

    Add files via upload

---
+++ Tarea-SQL-Server/SQL/64-Eliminar índices.sql
@@ -0,0 +1,22 @@
+if object_id('libros') is not null
+  drop table libros;
+
+ create table libros(
+  codigo int identity,
+  titulo varchar(40),
+  autor varchar(30),
+  editorial varchar(15)
+ );
+
+ create index I_libros_titulo
+ on libros(titulo);
+
+ sp_helpindex libros;
+
+ drop index libros.I_libros_titulo;
+
+ sp_helpindex libros;
+
+ if exists (select name from sysindexes
+  where name = 'I_libros_titulo')
+   drop index libros.I_libros_titulo;
