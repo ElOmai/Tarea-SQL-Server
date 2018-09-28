786a7e3dc81d1678864d7c92ccbbd910c6ee1e0b
ElOmai <43100537+ElOmai@users.noreply.github.com>
2018-09-28 10:50:32 -0400

    Add files via upload

---
+++ Tarea-SQL-Server/SQL/2- Crear una tabla.sql
@@ -0,0 +1,21 @@
+--2 - Crear una tabla (create table - sp_tables - sp_columns - drop table)
+ if object_id('usuarios') is not null
+  drop table usuarios;
+
+ create table usuarios (
+  nombre varchar(30),
+  clave varchar(10)
+ );
+
+ sp_tables @table_owner='dbo';
+
+ sp_columns usuarios;
+
+ create table usuarios (
+  nombre varchar(30),
+  clave varchar(10)
+ );
+
+ drop table usuarios;
+
+ sp_tables @table_owner='dbo';
